6360a4ddf77e3308e1a0ca239065011093606568
bpederse <bpederse@bf10c16d-a419-0410-831a-67fade187715>
2006-07-28 15:46:07 +0000

    in config.php NAME and VERSION were used before defined.

--- tools/config.php
+++ tools/config.php
@@ -1,10 +1,10 @@
 <?
 
 define('ENTRY_POINT', '/source/jam.js');
-define('OUTPUT_FILE', '/dist/'.strtolower(NAME.'-'.VERSION).'.js');
 
 define('NAME',         'JAM');
 define('VERSION',      '0.3-dev');
+define('OUTPUT_FILE', '/dist/'.strtolower(NAME.'-'.VERSION).'.js');
 define('BUILD',        'Standard');
 define('INCLUDE_PATH', WWW.'/?');
 
