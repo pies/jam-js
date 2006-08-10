3d6c4f40fca462b903702ce4eceb33428463ec19
tatarynowicz <tatarynowicz@bf10c16d-a419-0410-831a-67fade187715>
2006-08-10 13:07:04 +0000

    - merged index.php - added my working directory as /branches/pies

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ header('Content-type: text/javascript');
 define('OM_STATIC', 1);
 define('OM_DYNAMIC', 2);
 
-define('BUILD_NAME',  GET($_GET, 'name', 'full_build'));
+define('BUILD_NAME',  GET($_GET, 'name', 'full'));
 define('OUTPUT_MODE', GET($_GET, 'mode', 'static')=='dynamic'? OM_DYNAMIC: OM_STATIC);
 
 define('SOURCE_PATH', dirname(_ROOT.ENTRY_POINT));
 
-print render( $lib? SOURCE_PATH."/{$lib}_build.js": _ROOT.ENTRY_POINT );
+print render( SOURCE_PATH."/jam-".BUILD_NAME.".js" );
 ?>
